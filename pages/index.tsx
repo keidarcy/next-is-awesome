52512ce6b19442c92061802dbb71dd8e70413af5
XING YAHAO <keidarcy.1015@gmail.com>
2020-08-15 12:37:21 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>YZ IS 素晴らしい</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
