5533dc28f1a1dfb0a8a44c1576edc823ee447cca
XING YAHAO <keidarcy.1015@gmail.com>
2020-08-15 17:50:43 +0900

    shopify style

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,64 +1,45 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
+import SideBar from './SideBar'
+import Footer from './Footer'
+import Header from './Header'
 
-const name = 'YZNB'
-export const siteTitle = 'YZNB NEXTJS'
-const Layout = ({ children, home }: { children: React.ReactNode; home?: boolean }) => {
+const TopHero = () => (
+  <section className="section section--tight section-padding section--padding-top-only ui-hero">
+    <div className="grid">
+      <div className="grid__item">
+        <p className="section-heading__kicker heading--5 hide--mobile">
+          Shopify Open Source
+        </p>
+        <h1 className="section-heading__heading heading--2 ui-hero__header gutter-bottom--reset--mobile">
+          Shopify App CLI
+        </h1>
+        <p className="text-major hide--mobile ui-hero__header">
+          Build Shopify apps faster
+        </p>
+      </div>
+    </div>
+  </section>
+)
+
+const Layout = ({
+  children,
+  posts
+}: {
+  children: React.ReactNode
+  posts: { id: string; title: string; date: string }[]
+}) => {
   return (
     <>
-      <div className={styles.container}>
-        <Head>
-          <link ref="icon" href="/favicon.ico" />
-          <meta name="description" content="Learn" />
-          <meta
-            property="og:image"
-            content={`https://og-image.now.sh/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <header className={styles.header}>
-          {home ? (
-            <>
-              <img
-                src="/images/profile.jpg"
-                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                alt={name}
-              />
-              <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            </>
-          ) : (
-            <>
-              <Link href="/">
-                <a>
-                  <img
-                    src="/images/profile.jpg"
-                    className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                    alt={name}
-                  />
-                </a>
-              </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/">
-                  <a className={utilStyles.colorInherit}>{name}</a>
-                </Link>
-              </h2>
-            </>
-          )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-          <div className={styles.backToHome}>
-            <Link href="/">
-              <a>Back to Home</a>
-            </Link>
+      <Header />
+      <div id="PageContainer">
+        <TopHero />
+        <section id="Content">
+          <div className="grid">
+            <SideBar posts={posts} />
+            {children}
           </div>
-        )}
+        </section>
       </div>
+      <Footer />
     </>
   )
 }
